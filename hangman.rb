261cbc8f77f5f20986d9d0fef5dfcd03a9600f9d
LaurynBiggs <lauryn@biggs.email>
2021-12-07 11:10:40 -0500

    coditional code...still does not work

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,45 @@
-banner = "
-     _____
-    |     |
-    |     |
-     ¯¯|¯¯
-    \  |  /
-     \ | /
-       |
-      / \
-     /   \
-    -     -
+class Body_Parts
+  def initialize(head, arms_upper_body, legs_lower_body)
+    @head, @arms_upper_body, @legs_lower_body = head, arms_upper_body, legs_lower_body
+  end
+head = "
+  _____
+ | * * |
+ |  ^  |
+  ¯¯|¯¯
 "
-puts banner.gsub(/\\/, "\\\\")
+arms_upper_body = "    
+ \  |  /
+  \ | /
+"
+legs_lower_body = "
+    |
+   / \
+  /   \
+ -     -
+"
+end
 
-words = ["hat", "cool", "email"]
-puts words.shuffle
+word = "hat"
+puts "Welcome to Hangman!
+      The rules are simple:
+      type on a letter to see if it is correct
+      if correct, the letter appears on the screen
+      if not correct, a body part appears on the screen
+      win by not completing the whole man, enjoy!"
+
+puts "type a letter in"
+if gets.chomp == 'h'
+  puts "Alright! Put another letter in."
+  gets.chomp
+elsif gets.chomp == 'a'
+  puts "You are on a roll! Put another letter in."
+  gets.chomp
+elsif gets.chomp == 't'
+  puts "Woohoo! Put another letter in."
+  gets.chomp
+elsif gets.chomp != ['h', 'a', 't']
+  puts Body_Parts(@head)
+else gets.chomp == ['h', 'a', 't'] 
+  puts "Awesome! you spelled out the whole word, #{word}"
+end
