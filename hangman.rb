6adbec5ab66a5fb685bab3ba428dcb888da76b79
LaurynBiggs <lauryn@biggs.email>
2021-12-09 15:02:27 -0500

    new coditional logic

--- hangman.rb
+++ hangman.rb
@@ -1,45 +1,24 @@
-class Body_Parts
-  def initialize(head, arms_upper_body, legs_lower_body)
-    @head, @arms_upper_body, @legs_lower_body = head, arms_upper_body, legs_lower_body
-  end
-head = "
-  _____
- | * * |
- |  ^  |
-  ¯¯|¯¯
-"
-arms_upper_body = "    
- \  |  /
-  \ | /
-"
-legs_lower_body = "
-    |
-   / \
-  /   \
- -     -
-"
-end
-
 word = "hat"
 puts "Welcome to Hangman!
       The rules are simple:
       type on a letter to see if it is correct
       if correct, the letter appears on the screen
       if not correct, a body part appears on the screen
-      win by not completing the whole man, enjoy!"
-
-puts "type a letter in"
-if gets.chomp == 'h'
-  puts "Alright! Put another letter in."
-  gets.chomp
-elsif gets.chomp == 'a'
-  puts "You are on a roll! Put another letter in."
-  gets.chomp
-elsif gets.chomp == 't'
-  puts "Woohoo! Put another letter in."
-  gets.chomp
-elsif gets.chomp != ['h', 'a', 't']
-  puts Body_Parts(@head)
-else gets.chomp == ['h', 'a', 't'] 
-  puts "Awesome! you spelled out the whole word, #{word}"
+      win by not completing the whole man, enjoy! Type a letter in"
+person = gets.chomp
+if person == 'h'
+  person == true
+    puts "great job! now another."
+    gets.chomp
+elsif person == 'a'
+  person == true
+    puts "great job! now another."
+    gets.chomp
+elsif person == 't'
+  person == true
+    puts "great job! now another."
+    gets.chomp
+else person == false
+  artii = Ascii::Artii::Base.new([])
+  puts artii.asciify('(*-*)')
 end
