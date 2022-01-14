571045bccc335c138419ad0a372a285d15e76f1e
LaurynBiggs <lauryn@biggs.email>
2022-01-14 11:56:16 -0500

    Different method

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,7 @@
-word = "hat"
+alphabet = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l',
+'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R' 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X',
+'y', 'Y', 'z', 'Z']
+alphabet.values_at(0, 1, 4, 5, 38, 39)
 puts "Welcome to Hangman!
       The rules are simple:
       type on a letter to see if it is correct
@@ -6,19 +9,19 @@ puts "Welcome to Hangman!
       if not correct, a body part appears on the screen
       win by not completing the whole man, enjoy! Type a letter in"
 person = gets.chomp
-if person == 'h'
+if person == [4, 5]
   person == true
-    puts "great job! now another."
+    puts "great job! now another. c _ _"
     gets.chomp
-elsif person == 'a'
+elsif person == [0, 1]
   person == true
-    puts "great job! now another."
+    puts "great job! now another. _ a _"
     gets.chomp
-elsif person == 't'
+elsif person == [38, 39]
   person == true
-    puts "great job! now another."
+    puts "great job! now another. _ _ t"
     gets.chomp
-else person == false
-  artii = Ascii::Artii::Base.new([])
-  puts artii.asciify('(*-*)')
+else person != alphabet.values_at(0, 1, 4, 5, 38, 39)
+  person == false
+    puts "Wrong! Try again : #{person}"
 end
