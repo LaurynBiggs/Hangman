ffd75c21f9a129a6fdbf93c12c3ded5bca185397
LaurynBiggs <lauryn@biggs.email>
2022-03-01 12:26:18 -0500

    made case statement. says wrong, try again for every time input is given

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,4 @@
+# first, write the rules
 puts "Welcome to Hangman!
       The rules are simple:
       Type a letter in to see if it is correct.
@@ -6,16 +7,22 @@ puts "Welcome to Hangman!
       Win completing the whole word, lose by completing the whole man, enjoy!
       Note: no letter repeats itself in this game and there are three letters. Start typin' a letter in"
 
-def user
-      for a in 0..2 do
-            gets.chomp
-            puts "correct, now another" 
-      end
+# second, make a for loop so it can go the letters
+for str in 0..2 do
+      gets.chomp
+# third, process the letters and see if they match the predefined word.
+      case str
+            when "a"
+                  puts "correct, now another"
+            when "c"
+                  puts "correct, now another"
+            when "t"
+                  puts "correct, now another"
+            else 
+                  puts "wrong, try again"
+            end
 end
-user
-# if    user == 'a' or user == 'A'
-      # elsif user == 'c' or user == 'C'
-      # else user == 't' or user == 'T'
-      # else  user.to_s.empty?
-      #       puts 'you didnt even put a word in, try again'
-# end
+# fourth and final, display ASCII art to the screen if the letters are wrong.
+
+# if you did these steps correctly Lauryn,
+# then congrats! You succesfully made a hangman game.
